735fa2832653ac79264119e6ba05e2b490ecf549
Pedro <engpedrocampos@gmail.com>
2020-11-14 13:58:10 -0300

    First Commit

---
+++ src/main/java/br/com/restwithspringbootarchitecture/restwithspringbootarchitecture/RestWithSpringBootArchitectureApplication.java
@@ -0,0 +1,13 @@
+package br.com.restwithspringbootarchitecture.restwithspringbootarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestWithSpringBootArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestWithSpringBootArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/restwithspringbootarchitecture/restwithspringbootarchitecture/RestWithSpringBootArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.restwithspringbootarchitecture.restwithspringbootarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestWithSpringBootArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
