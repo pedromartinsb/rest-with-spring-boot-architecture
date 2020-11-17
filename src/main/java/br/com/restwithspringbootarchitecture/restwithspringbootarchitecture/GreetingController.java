59ca6478481f38245f120b22d7b017e63dc63c2a
Pedro <engpedrocampos@gmail.com>
2020-11-17 10:02:40 -0300

    Adicionando system properties

---
+++ src/main/java/br/com/restwithspringbootarchitecture/restwithspringbootarchitecture/GreetingController.java
@@ -0,0 +1,22 @@
+package br.com.restwithspringbootarchitecture.restwithspringbootarchitecture;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class GreetingController {
+
+    private final String template = "Hello, %s!";
+
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping(value = "/sum/numberOne/numberTwo", method = RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        return 1D;
+    }
+
+}
